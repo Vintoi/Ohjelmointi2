d0147ac6ce8375215db284af6f944d4228899c3c
Oskari <oskari.kolehmainen@tuni.fi>
2021-01-14 13:05:34 +0200

    Numerotehtävä Korjailtu ulkoasua ja poistettu turhia muuttujia

--- student/02/factors/main.cpp
+++ student/02/factors/main.cpp
@@ -7,8 +7,6 @@ int main()
     cout << "Enter a positive number: ";
     int num = 0;
     std::cin >> num;
-    int jakaja = 0;
-
     int pienin = 1;
     int erotus = num/1 -1;
 
@@ -24,10 +22,8 @@ int main()
                 }
                     //cout << erotus << " " << pienin <<" " << i << endl;
                 }
-
         }
         cout << num <<" = " << num/pienin << " * "<< pienin << endl;
     }
-
     return 0;
 }
