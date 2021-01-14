bd39d93d9f240aba032e9a8f4d8e12932359f7a2
Oskari <oskari.kolehmainen@tuni.fi>
2021-01-14 12:59:32 +0200

    Numerotehtävä Lisättiin loop joka etsii pienimmän erotuksen ja ilmoittaa tulon tekijät

--- student/02/factors/main.cpp
+++ student/02/factors/main.cpp
@@ -7,17 +7,27 @@ int main()
     cout << "Enter a positive number: ";
     int num = 0;
     std::cin >> num;
-    int eka = 0;
-    //int toka = 2;
-    if(num % 2 == 0) {
-        eka = num / 2;
-    }
+    int jakaja = 0;
 
+    int pienin = 1;
+    int erotus = num/1 -1;
 
     if(num <= 0){
         cout << "Only positive numbers accepted" << endl;
     } else {
-        cout << num <<" = " << eka << " * "<< 2 << endl;
+        for (int i = 1; i <= num; ++i) {
+            if(num % i == 0){
+                if ((num/i-i) < 0){
+                } else if ((num/i -i) < erotus){
+                    erotus = num/i - i;
+                    pienin = i;
+                }
+                    //cout << erotus << " " << pienin <<" " << i << endl;
+                }
+
+        }
+        cout << num <<" = " << num/pienin << " * "<< pienin << endl;
     }
+
     return 0;
 }
