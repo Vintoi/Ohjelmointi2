9fb1f908f69dbb5c686834eecab9393670b16bcc
Oskari <oskari.kolehmainen@tuni.fi>
2021-01-14 10:50:49 +0200

    Numeropeli Lisätty 7 jaollisten huomioiminen

--- student/02/number_series_game/main.cpp
+++ student/02/number_series_game/main.cpp
@@ -8,8 +8,12 @@ int main()
     std::cin >> num;
 
     for (int i = 1; i <= num; ++i){
-        if (i % 3 == 0) {
+        if (i % 3 == 0 && i % 7 == 0){
+            std::cout << "zip boing" << std::endl;
+        }else if (i % 3 == 0) {
             std::cout << "zip" << std::endl;
+        } else if (i % 7 == 0) {
+            std::cout << "boing" << std::endl;
         } else {
             std::cout << i << std::endl;
         }
