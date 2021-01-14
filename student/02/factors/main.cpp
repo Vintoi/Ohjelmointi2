9b2e634622d3cc77a9c88ad4ebbb6d12d5472996
Oskari <oskari.kolehmaien@tuni.fi>
2021-01-14 11:04:07 +0200

    H2 Parillisten jako testi

---
+++ student/02/factors/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Enter a positive number: ";
+    int num = 0;
+    std::cin >> num;
+    int eka = 0;
+    int toka = 2;
+    if(num % 2 == 0) {
+        eka = num / 2;
+    }
+
+
+    if(num <= 0){
+        cout << "Only positive numbers accepted" << endl;
+    } else {
+        cout << num <<" = " << eka << " * "<< 2 << endl;
+    }
+    return 0;
+}
