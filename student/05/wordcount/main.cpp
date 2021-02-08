3152ed076635569ce05197fce068c3b80ece644a
Oskari <shosko@linux-desktop1.tuni.fi>
2021-02-08 21:59:34 +0200

    Wordcount COmmit, jotta pystyy testaamaan Plussassa toiminnallisuuksia

---
+++ student/05/wordcount/main.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <map>
+#include <vector>
+#include <set>
+
+using namespace std;
+std::vector<std::string> split(const std::string& s, const char delimiter, bool ignore_empty = false){
+    std::vector<std::string> result;
+    std::string tmp = s;
+
+    while(tmp.find(delimiter) != std::string::npos)
+    {
+        std::string new_part = tmp.substr(0, tmp.find(delimiter));
+        tmp = tmp.substr(tmp.find(delimiter)+1, tmp.size());
+        if(not (ignore_empty and new_part.empty()))
+        {
+            result.push_back(new_part);
+        }
+    }
+    if(not (ignore_empty and tmp.empty()))
+    {
+        result.push_back(tmp);
+    }
+    return result;
+}
+
+
+
+int main()
+{
+    map<string, set <int>> tulokset;
+
+    string input_file = "";
+    cout << "Input file: ";
+    getline(cin, input_file);
+
+    ifstream input_olio;
+    //ofstream output_olio;
+
+    input_olio.open(input_file);
+    if (not input_olio){
+        //output_olio.close();
+        cout << "Error! The file " << input_file << " cannot be opened." << endl;
+        input_olio.close();
+        return EXIT_FAILURE;
+    } else {
+        string rivi,sana;
+        int rivi_nro;
+        while (getline(input_olio,rivi)){
+            vector<string> rivilta = split(rivi,' ');
+            for (int i = 0; i < int(rivilta.size()); i++){
+                sana = rivilta.at(i);
+                if(tulokset.find(sana) != tulokset.end()){
+                    tulokset[sana].insert(rivi_nro);
+                } else {
+                    tulokset[sana].insert(rivi_nro);
+                }
+            }
+            rivi_nro++;
+            rivilta.clear();
+        }
+    }
+    for (auto tulos : tulokset){
+        cout << tulos.first << " "<< tulos.second.size() << ": " << endl;
+    }
+
+}
